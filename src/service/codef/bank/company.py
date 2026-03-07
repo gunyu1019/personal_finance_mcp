af3816e6023e2b6fc8af4fa3794f1226047a5312
gunyu1019 <gunyu1019@gmail.com>
2026-03-08 03:09:20 +0900

    [Feat] Add Company enum with bank, credit card identifiers

---
+++ src/service/codef/bank/company.py
@@ -0,0 +1,24 @@
+from enum import IntEnum
+
+
+class Company(IntEnum):
+    KDB = 2
+    IBK = 3
+    KB = 4
+    SUHYUP = 7
+    NH = 11
+    WOORI = 20
+    SC = 23
+    CITI = 27
+    DAEGU = 31
+    BUSAN = 32
+    GWANGJU = 34
+    JEJU = 35
+    JEONBUK = 37
+    GYEONGNAM = 39
+    SAEMAEUL = 45
+    CU = 48
+    POST = 71
+    KEB_HANA = 81
+    SHINHAN = 88
+    K_BANK = 89  # Deprecated
---
+++ src/service/codef/card/company.py
@@ -0,0 +1,18 @@
+from enum import IntEnum
+
+
+class Company(IntEnum):
+    KB = 301
+    HYUNDAI = 302
+    SAMSUNG = 303
+    NH = 304
+    BC = 305
+    SHINHAN = 306
+    CITI = 307
+    WOORI = 309
+    LOTTE = 311
+    HANA = 313
+    JEONBUK = 315
+    GWANGJU = 316
+    SUHYUP = 320
+    JEJU = 321
