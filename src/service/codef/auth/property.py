dccc3aaed8aa4735c5d95d0e5f74b7f0d5b7ee23
gunyu1019 <gunyu1019@gmail.com>
2026-03-09 04:01:14 +0900

    [Feat] Add API endpoint paths and OAuth domain configurations for account management

---
+++ src/service/codef/auth/property.py
@@ -0,0 +1,6 @@
+PATH_CREATE_ACCOUNT = '/v1/account/create'
+PATH_ADD_ACCOUNT = '/v1/account/add'
+PATH_UPDATE_ACCOUNT = '/v1/account/update'
+PATH_DELETE_ACCOUNT = '/v1/account/delete'
+PATH_GET_ACCOUNT_LIST = '/v1/account/list'
+PATH_GET_CID_LIST = '/v1/account/connectedId-list'
---
+++ src/service/codef/property.py
@@ -0,0 +1,9 @@
+OAUTH_DOMAIN = 'https://oauth.codef.io'
+DEMO_DOMAIN = 'https://development.codef.io'
+API_DOMAIN = 'https://api.codef.io'
+SANDBOX_DOMAIN = 'https://sandbox.codef.io'
+
+PATH_GET_TOKEN = '/oauth/token'
+
+SANDBOX_CLIENT_ID = 'ef27cfaa-10c1-4470-adac-60ba476273f9'
+SANDBOX_CLIENT_SECRET = '83160c33-9045-4915-86d8-809473cdf5c3'
