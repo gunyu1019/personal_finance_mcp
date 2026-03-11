47f6dcf4622458230e42ecd4c70021f072e18e7f
gunyu1019 <gunyu1019@gmail.com>
2026-03-12 02:09:46 +0900

    [Feat] Add Account model and modify AccountResult structure for account management

---
+++ src/service/codef/auth/account.py
@@ -0,0 +1,14 @@
+from pydantic import Field
+from typing import Optional, Literal
+
+from ..base_model import CodefBaseModel
+
+
+class Account(CodefBaseModel):
+    code: Optional[str] = Field(None, exclude=True)  # 결과코드
+    message: Optional[str] = Field(None, exclude=True)   # 메시지
+    country_code: Literal['KR']  # 국가코드 (한국: KR)
+    client_type: Literal['P', 'B', 'A']  # 고객 구분 (개인: P, 기업/법인: B, 통합: A)
+    organization: str  # 기관코드
+    business_type: Literal['BK', 'CD', 'ST', 'IS']  # 업무 구분 (은행/저축은행: BK, 카드: CD, 증권: ST, 보험: IS)
+    login_type: Optional[Literal['0', '1']] = None # 로그인 방식 (0: 인증서, 1: 아이디/패스워드) / 계정 삭제 시에만 사용
--- src/service/codef/auth/account_result.py
+++ src/service/codef/auth/account_result.py
@@ -1,18 +1,20 @@
-from typing import Optional, Literal
+from .account import Account
 from ..base_model import CodefBaseModel
 
 
-class Account(CodefBaseModel):
-    code: Optional[str] = None  # 결과코드
-    message: Optional[str] = None  # 메시지
-    country_code: Literal['KR']  # 국가코드 (한국: KR)
-    client_type: Literal['P', 'B', 'A']  # 고객 구분 (개인: P, 기업/법인: B, 통합: A)
-    organization: str  # 기관코드
-    business_type: Literal['BK', 'CD', 'ST', 'IS']  # 업무 구분 (은행/저축은행: BK, 카드: CD, 증권: ST, 보험: IS)
-
-
-
-class AccountResult(CodefBaseModel):
+class AccountModifyResult(CodefBaseModel):
     connected_id: str  # 커넥티드 아이디
     success_list: list[Account]
     error_list: list[Account]
+
+
+class AccountListResult(CodefBaseModel):
+    connected_id: str  # 커넥티드 아이디
+    account_list: list[Account]
+
+
+class ConnectedIdListResult(CodefBaseModel):
+    page_no: int
+    has_next: bool
+    connected_id_list: list[str]
+    next_page_no: int
