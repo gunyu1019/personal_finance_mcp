6872b9b6ed6c5887cd9d0864f1d1ddd147e1e3be
gunyu1019 <gunyu1019@gmail.com>
2026-03-12 02:09:52 +0900

    [Feat] Update account-related API methods to use AccountModifyResult and enhance request/response models

--- src/service/codef/auth/http.py
+++ src/service/codef/auth/http.py
@@ -1,14 +1,15 @@
 import asyncio
 
 from ahttp_client import BodyJson, request
-from ahttp_client.extension import pydantic_response_model
+from ahttp_client.extension import pydantic_response_model, pydantic_request_model
 from typing import Annotated, Optional
 
 from ..base_http import CodefBaseHttp
 from ..result import CodefResult
 
+from .account import Account
 from .account_register import AccountRegister
-from .account_result import AccountResult, Account
+from .account_result import AccountModifyResult, ConnectedIdListResult, AccountListResult
 from .property import *
 
 
@@ -17,46 +18,52 @@ class AuthHttp(CodefBaseHttp):
                  loop: Optional[asyncio.AbstractEventLoop] = None):
         super().__init__(base_url, client_id, client_secret, loop)
 
+    @pydantic_request_model()
     @pydantic_response_model()
     @request("POST", PATH_CREATE_ACCOUNT)
     async def create_account(
             self,
             account: Annotated[list[AccountRegister], BodyJson.custom_name("accountList")]
-    ) -> CodefResult[AccountResult]:
+    ) -> CodefResult[AccountModifyResult]:
         pass
 
+    @pydantic_request_model()
     @pydantic_response_model()
     @request("POST", PATH_ADD_ACCOUNT)
     async def add_account(
             self,
             connected_id: Annotated[str, BodyJson.to_camel()],
             account: Annotated[list[AccountRegister], BodyJson.custom_name("accountList")]
-    ) -> CodefResult[AccountResult]:
+    ) -> CodefResult[AccountModifyResult]:
         pass
 
+    @pydantic_request_model()
     @pydantic_response_model()
     @request("POST", PATH_UPDATE_ACCOUNT)
     async def update_account(
             self,
             connected_id: Annotated[str, BodyJson.to_camel()],
             account: Annotated[list[AccountRegister], BodyJson.custom_name("accountList")]
-    ) -> CodefResult[AccountResult]:
+    ) -> CodefResult[AccountModifyResult]:
         pass
 
+    @pydantic_request_model()
     @pydantic_response_model()
     @request("POST", PATH_DELETE_ACCOUNT)
     async def delete_account(
             self,
             account: Annotated[list[Account], BodyJson.custom_name("accountList")]
-    ) -> CodefResult[AccountResult]:
+    ) -> CodefResult[AccountModifyResult]:
         pass
 
+    @pydantic_request_model()
     @pydantic_response_model()
     @request("POST", PATH_GET_ACCOUNT_LIST)
-    async def get_account_list(self, connected_id: Annotated[str, BodyJson.to_camel()]) -> dict:
+    async def get_account_list(self, connected_id: Annotated[str, BodyJson.to_camel()]) -> CodefResult[AccountListResult]:
         pass
 
+    @pydantic_request_model()
     @pydantic_response_model()
     @request("POST", PATH_GET_CID_LIST)
-    async def get_cid_list(self, page_no: Annotated[int, BodyJson.to_camel()]) -> dict:
+    async def get_cid_list(self, page_no: Annotated[int, BodyJson.to_camel()]) -> CodefResult[ConnectedIdListResult]:
         pass
