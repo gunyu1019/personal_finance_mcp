405c1ac5c3060626814271dc4ad449d0951d4df1
gunyu1019 <gunyu1019@gmail.com>
2026-03-13 03:56:03 +0900

    [Feat] Implement CardHttp class with methods for card registration status, account list, and approval list

---
+++ src/service/codef/card/property.py
@@ -0,0 +1,3 @@
+PATH_CARD_REGISTRATION_STATUS = "/v1/kr/card/p/user/registration-status"
+PATH_CARD_ACCOUNT_LIST = "/v1/kr/card/p/account/card-list"
+PATH_CARD_APPROVAL_LIST = "/v1/kr/card/p/account/approval-list"
