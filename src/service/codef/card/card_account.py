dace205f16ceadb9078dd3a375496fe4ec1e8bd8
gunyu1019 <gunyu1019@gmail.com>
2026-03-13 03:55:53 +0900

    [Feat] Add CardAccount model for managing card account details

---
+++ src/service/codef/card/card_account.py
@@ -0,0 +1,15 @@
+from typing import Literal, Optional
+
+from ..base_model import CodefBaseModel
+
+
+class CardAccount(CodefBaseModel):
+    res_card_no: str
+    res_sleep_yn: Optional[Literal['Y', 'N']] = None
+    res_card_name: str
+    res_card_type: Optional[str] = None
+    res_traffic_yn: Optional[Literal['Y', 'N']] = None
+    res_image_link: Optional[str] = None
+    res_issue_date: Optional[str] = None
+    res_valid_period: Optional[str] = None
+    res_status: Optional[str] = None
