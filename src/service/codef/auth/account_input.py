6e6e57ebc1f6c3b99e467060d213fc763285331e
gunyu1019 <gunyu1019@gmail.com>
2026-03-14 15:27:35 +0900

    [Feat] Add AccountRegisterList, AccountModifyList, and AccountList models for account management

---
+++ src/service/codef/auth/account_input.py
@@ -0,0 +1,18 @@
+from pydantic import Field
+
+from .account import Account
+from .account_register import AccountRegister
+from ..base_model import CodefBaseModel
+
+
+class AccountRegisterList(CodefBaseModel):
+    account_list: list[AccountRegister] = Field(default_factory=list)
+
+
+class AccountModifyList(CodefBaseModel):
+    account_list: list[AccountRegister] = Field(default_factory=list)
+    connected_id: str
+
+
+class AccountList(CodefBaseModel):
+    account_list: list[Account] = Field(default_factory=list)
