eb6275c319896882251553092804e86417c4c82e
gunyu1019 <gunyu1019@gmail.com>
2026-03-14 15:28:20 +0900

    [Fix] Change Company class to StrEnum with 4-digit string codes for banks and card companies

--- src/service/codef/card/company.py
+++ src/service/codef/card/company.py
@@ -1,18 +1,19 @@
-from enum import IntEnum
+from enum import StrEnum
 
 
-class Company(IntEnum):
-    KB = 301
-    HYUNDAI = 302
-    SAMSUNG = 303
-    NH = 304
-    BC = 305
-    SHINHAN = 306
-    CITI = 307
-    WOORI = 309
-    LOTTE = 311
-    HANA = 313
-    JEONBUK = 315
-    GWANGJU = 316
-    SUHYUP = 320
-    JEJU = 321
+class Company(StrEnum):
+    """카드사 코드 (4자리 문자열)"""
+    KB = "0301"
+    HYUNDAI = "0302"
+    SAMSUNG = "0303"
+    NH = "0304"
+    BC = "0305"
+    SHINHAN = "0306"
+    CITI = "0307"
+    WOORI = "0309"
+    LOTTE = "0311"
+    HANA = "0313"
+    JEONBUK = "0315"
+    GWANGJU = "0316"
+    SUHYUP = "0320"
+    JEJU = "0321"
