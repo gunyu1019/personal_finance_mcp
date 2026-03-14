6e5e710abb141c06c3c83d56249b5e763a74e4b9
gunyu1019 <gunyu1019@gmail.com>
2026-03-14 15:28:11 +0900

    [Fix] Update encryption method to use DER format and PKCS1v15 padding for improved compatibility

--- src/service/codef/encryption.py
+++ src/service/codef/encryption.py
@@ -2,19 +2,17 @@ import base64
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.serialization import load_der_public_key
+
 
 def get_encrypt(value: str, public_key_pem: str) -> str:
-    public_key = serialization.load_pem_public_key(
-        public_key_pem.encode('utf-8')
-    )
+    key_der = base64.b64decode(public_key_pem)
 
-    encrypted_bytes = public_key.encrypt(
-        value.encode('utf-8'),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
+    public_key_obj = load_der_public_key(key_der)
+
+    encrypted_bytes = public_key_obj.encrypt(
+        value.encode(),
+        padding.PKCS1v15(),
     )
 
     encrypted_base64 = base64.b64encode(encrypted_bytes).decode('utf-8')
